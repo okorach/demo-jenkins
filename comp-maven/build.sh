676586d5ab0cbe8ba2fd442214be3b90ebd82414
Olivier Korach <olivier.korach@gmail.com>
2021-10-12 16:49:41 +0200

    First commit

---
+++ comp-maven/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Load common environment
+
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report \
+   -Dmaven.test.failure.ignore=true \
+   sonar:sonar \
+   -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN \
+   -Dsonar.exclusions=pom.xml \
+   -Dsonar.projectKey="demo:github-comp-maven" -Dsonar.projectName="GitHub project - Maven" \
+   $*
+
+exit $?
